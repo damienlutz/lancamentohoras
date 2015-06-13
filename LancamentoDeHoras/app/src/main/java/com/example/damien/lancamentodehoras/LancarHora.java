d8e68d28a4e9af6227d678c5ebb17f9aa085f39f
Damien <damien@damien-Inspiron-5423.(none)>
2015-06-13 15:03:27 -0300

    funcionando

--- LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/LancarHora.java
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/LancarHora.java
@@ -21,31 +21,11 @@ import android.widget.Button;
  * create an instance of this fragment.
  */
 public class LancarHora extends android.support.v4.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LancarHora.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LancarHora newInstance(String param1, String param2) {
+
+    public static LancarHora newInstance() {
         LancarHora fragment = new LancarHora();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         Log.d(TAG, "log new inst");
         return fragment;
     }
@@ -59,10 +39,6 @@ public class LancarHora extends android.support.v4.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         Log.d(TAG, "oncreate ");
     }
