262f1630784f6b2d3cc9e4aa7efec336c9593685
Damien <damien.lutz@ilegra.com>
2015-07-06 23:08:36 -0300

    afd

--- LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/LancamentoDeHorasActivity.java
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/LancamentoDeHorasActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.damien.lancamentodehoras.home.HomeActivity;
 
