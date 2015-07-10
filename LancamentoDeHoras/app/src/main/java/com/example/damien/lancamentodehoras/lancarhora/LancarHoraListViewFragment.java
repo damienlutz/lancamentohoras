5c3f4070c0f8687dfea43dcd1b3ff588c24af565
damienlutz <damien.lutz@ilegra.com>
2015-07-10 13:23:22 -0300

    Update LancarHoraListViewFragment.java

--- LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/lancarhora/LancarHoraListViewFragment.java
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/lancarhora/LancarHoraListViewFragment.java
@@ -16,8 +16,9 @@ public class LancarHoraListViewFragment extends ListFragment {
     String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
         "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
         "Linux", "OS/2" };
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-        android.R.layout.simple_list_item_1, values);
+        
+        
+    MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
     setListAdapter(adapter);
   }
 
