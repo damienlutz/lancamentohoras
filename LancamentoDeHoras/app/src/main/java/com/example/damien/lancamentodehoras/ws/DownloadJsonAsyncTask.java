a8a593f49bdeff95753ce5054b90bb6c9bdbdcba
Damien <damien.lutz@ilegra.com>
2015-07-17 13:27:38 -0300

    ws

---
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/DownloadJsonAsyncTask.java
@@ -0,0 +1,7 @@
+package com.example.damien.lancamentodehoras.ws;
+
+/**
+ * Created by damien on 17/07/15.
+ */
+public class DownloadJsonAsyncTask {
+}
---
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/LancarHoraWS.java
@@ -0,0 +1,8 @@
+package com.example.damien.lancamentodehoras.ws;
+
+/**
+ * Created by damien on 17/07/15.
+ */
+public class LancarHoraWS {
+
+}
