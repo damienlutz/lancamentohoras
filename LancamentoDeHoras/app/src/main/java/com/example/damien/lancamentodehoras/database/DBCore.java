453f4717e5e8131f050b416c63d94161f912bafe
Francisco Nelsis <fhnelsis@outlook.com>
2015-07-18 23:10:51 -0300

    12

--- LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/database/DBCore.java
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/database/DBCore.java
@@ -13,60 +13,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DBCore extends SQLiteOpenHelper {
-    private SQLiteDatabase db;
+    SQLiteDatabase abreBanco;
+    SQLiteOpenHelper openHelper;
     private static final String NOME_BD = "lancamentodehoras.db";
     private static final int VERSAO_BD = 1;
     private static final String NOME_TABLE = "lancamentos";
     private static final String CREATE_TABLE = "create table " + NOME_TABLE + "(_id integer primary key autoincrement, usuario text, horas text, projeto text, email text);";
     private static final String DROP_TABLE = "drop table " + NOME_TABLE + ";";
 
-
+    //UMA CLASSE OPENHELPER PARA CADA TABELA
     public DBCore(Context ctx) {
         super(ctx, NOME_BD, null, VERSAO_BD);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE);
-        db = this.getWritableDatabase();
-
+    public void onCreate(SQLiteDatabase abreBanco) {
+        abreBanco.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase bd, int arg1, int arg2) {
-        bd.execSQL("drop table " + NOME_TABLE + ";");
-        onCreate(bd);
+        abreBanco.execSQL("drop table " + NOME_TABLE + ";");
+        onCreate(abreBanco);
     }
 
     public void inserir(Lancamentos lancamentos) {
+        abreBanco = SQLiteDatabase.openOrCreateDatabase(NOME_BD, null, null);
+        abreBanco = openHelper.getWritableDatabase();
         ContentValues valores = new ContentValues();
         valores.put("usuario", lancamentos.getUsuarioLancamento());
         valores.put("horas", lancamentos.getHorasLancamentos());
         //valores.put("projeto", lancamentos.getProjetoLancamento());
         //valores.put("email", lancamentos.getEmailLancamento());
-        db.insert("lancamentos", null, valores);
+        abreBanco.insert("lancamentos", null, valores);
     }
 
     public List<Lancamentos> buscar() {
+        abreBanco = openHelper.getReadableDatabase();
         List<Lancamentos> list = new ArrayList<Lancamentos>();
         String[] colunas = new String[]{"_id", "usuario", "horas", "projeto", "email"};
 
-        Cursor cursor = db.query("lancamentos", colunas, null, null, null, null, "usuario ASC");
+        Cursor cursor = abreBanco.query("lancamentos", colunas, null, null, null, null, "usuario ASC");
 
-        if (cursor.getCount() > 0) {
-            cursor.moveToFirst();
+        //////logica da busca
 
-            do {
-                Lancamentos u = new Lancamentos();
-                u.set_id(cursor.getLong(0));
-                u.setUsuarioLancamento(cursor.getString(1));
-                u.setHorasLancamentos(cursor.getString(2));
-                u.setProjetoLancamento(cursor.getString(3));
-                u.setEmailLancamento(cursor.getString(4));
-                list.add(u);
-
-            } while (cursor.moveToNext());
-        }
         return (list);
     }
 }
