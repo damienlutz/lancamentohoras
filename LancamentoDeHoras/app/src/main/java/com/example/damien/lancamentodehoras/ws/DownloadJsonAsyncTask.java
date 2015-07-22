773f96a224c430d8cf892625dfa21d80942cf56e
damienlutz <damien.lutz@ilegra.com>
2015-07-22 13:22:59 -0300

    Update DownloadJsonAsyncTask.java

--- LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/DownloadJsonAsyncTask.java
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/DownloadJsonAsyncTask.java
@@ -27,7 +27,7 @@ import java.util.List;
 /**
  * Created by damien on 17/07/15.
  */
-public class DownloadJsonAsyncTask extends AsyncTask<String, Void, List<Trend>> {
+public class DownloadJsonAsyncTask extends AsyncTask<String, String, String> {
     ProgressDialog dialog;
 
     Context contextActivity;
@@ -41,69 +41,34 @@ public class DownloadJsonAsyncTask extends AsyncTask<String, Void, List<Trend>> 
         super.onPreExecute();
         dialog = ProgressDialog.show(contextActivity, "Aguarde", "Baixando JSON, Por Favor Aguarde...");
     }
-
-    @Override
-    protected List<Trend> doInBackground(String... params) {
-        String urlString = params[0];
+ @Override
+    protected String doInBackground(String... uri) {
         HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet(urlString);
+        HttpResponse response;
+        String responseString = null;
         try {
-            HttpResponse response = httpclient.execute(httpget);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                InputStream instream = entity.getContent();
-                String json = toString(instream);
-                instream.close();
-                List<Trend> trends = getTrends(json);
-                return trends;
+            response = httpclient.execute(new HttpGet(uri[0]));
+            StatusLine statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                responseString = out.toString();
+                out.close();
+            } else{
+                //Closes the connection.
+                response.getEntity().getContent().close();
+                throw new IOException(statusLine.getReasonPhrase());
             }
-        } catch (Exception e) {
-            Log.e("DEVMEDIA", "Falha ao acessar Web service", e);
+        } catch (ClientProtocolException e) {
+            //TODO Handle problems..
+        } catch (IOException e) {
+            //TODO Handle problems..
         }
-        return null;
-    }
-
-    private List<Trend> getTrends(String jsonString) {
-        List<Trend> trends = new ArrayList<Trend>();
-        try {
-            JSONArray trendLists = new JSONArray(jsonString);
-            JSONObject trendList = trendLists.getJSONObject(0);
-            JSONArray trendsArray = trendList.getJSONArray("trends");
-            JSONObject trend;
-            for (int i = 0; i < trendsArray.length(); i++) {
-                trend = new JSONObject(trendsArray.getString(i));
-                Log.i("DEVMEDIA", "nome=" + trend.getString("name"));
-                Trend objetoTrend = new Trend();
-                objetoTrend.name = trend.getString("name");
-                objetoTrend.url = trend.getString("url");
-                trends.add(objetoTrend);
-            }
-        } catch (JSONException e) {
-            Log.e("DEVMEDIA", "Erro no parsing do JSON", e);
-        }
-        return trends;
-    }
-
+        return responseString;
+    
     @Override
-    protected void onPostExecute(List<Trend> result) {
+    protected void onPostExecute(String result) {
         super.onPostExecute(result);
-        dialog.dismiss();
-        if (result.size() > 0) {
-            ArrayAdapter<Trend> adapter = new ArrayAdapter<Trend>(contextActivity, android.R.layout.simple_list_item_1, result);
-            //setListAdapter(adapter);
-        } else {
-            AlertDialog.Builder builder = new AlertDialog.Builder(contextActivity).setTitle("Atenção").setMessage("Não foi possivel acessar essas informções...").setPositiveButton("OK", null);
-            builder.create().show();
-        }
-    }
-
-    private String toString(InputStream is) throws IOException {
-        byte[] bytes = new byte[1024];
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        int lidos;
-        while ((lidos = is.read(bytes)) > 0) {
-            baos.write(bytes, 0, lidos);
-        }
-        return new String(baos.toByteArray());
+        //Do anything with response..
     }
 }
