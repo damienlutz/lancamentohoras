164a8a8f171fc80c19c4339e23f89ecf937f66dc
damienlutz <damien.lutz@ilegra.com>
2015-07-22 14:03:43 -0300

    Update DownloadJsonAsyncTask.java

--- LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/DownloadJsonAsyncTask.java
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/DownloadJsonAsyncTask.java
@@ -127,4 +127,16 @@ public class DownloadJsonAsyncTask extends AsyncTask<String, Void, String> {
 
      return http;
     }
+    
+private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+    BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+    String line = "";
+    String result = "";
+    while((line = bufferedReader.readLine()) != null)
+        result += line;
+
+    inputStream.close();
+    return result;
+
+}
 }
