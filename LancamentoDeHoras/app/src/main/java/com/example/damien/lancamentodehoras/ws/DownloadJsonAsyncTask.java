80ae414a0bc110a467e7aaa982e7807cf374d41d
Francisco Nelsis <fhnelsis@outlook.com>
2015-07-22 14:05:44 -0300

    .

--- LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/DownloadJsonAsyncTask.java
+++ LancamentoDeHoras/app/src/main/java/com/example/damien/lancamentodehoras/ws/DownloadJsonAsyncTask.java
@@ -6,33 +6,51 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.example.damien.lancamentodehoras.home.HomeActivity;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by damien on 17/07/15.
  */
+@SuppressWarnings("deprecation")
 public class DownloadJsonAsyncTask extends AsyncTask<String, Void, String> {
+    private static final String TAG = "WS";
     ProgressDialog dialog;
 
     Context contextActivity;
 
-    public  DownloadJsonAsyncTask ( Context contextActivity){
+    public DownloadJsonAsyncTask(Context contextActivity) {
         this.contextActivity = contextActivity;
     }
 
@@ -41,70 +59,71 @@ public class DownloadJsonAsyncTask extends AsyncTask<String, Void, String> {
         super.onPreExecute();
         dialog = ProgressDialog.show(contextActivity, "Aguarde", "Baixando JSON, Por Favor Aguarde...");
     }
- @Override
+
+    @Override
     protected String doInBackground(String... uri) {
         InputStream inputStream = null;
-    String result = "";
-    try {
+        String result = "";
+        try {
 
-        // 1. create HttpClient
-        HttpClient httpclient = getNewHttpClient();
+            // 1. create HttpClient
+            HttpClient httpclient = getNewHttpClient();
 
-        // 2. make POST request to the given URL
-        HttpPost httpPost = new HttpPost(uri[0]);
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(uri[0]);
 
-        String json = "";
+            String json = "";
 
-        // 3. build jsonObject
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.accumulate("user", 1);
-        jsonObject.accumulate("student_id", 1);
-        jsonObject.accumulate("user_email", "test@test.com");
-        jsonObject.accumulate("from", "Fri Oct 10 12:38:00 2014 GMT+0200");
-        jsonObject.accumulate("to", "Sat Oct 11 12:38:00 2014 GMT+0200");
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("user", 1);
+            jsonObject.accumulate("student_id", 1);
+            jsonObject.accumulate("user_email", "test@test.com");
+            jsonObject.accumulate("from", "Fri Oct 10 12:38:00 2014 GMT+0200");
+            jsonObject.accumulate("to", "Sat Oct 11 12:38:00 2014 GMT+0200");
 
-        // 4. convert JSONObject to JSON to String
-        json = jsonObject.toString();
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
 
-        // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-        // ObjectMapper mapper = new ObjectMapper();
-        // json = mapper.writeValueAsString(person);
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
 
-        // 5. set json to StringEntity
-        StringEntity se = new StringEntity(json);
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
 
-        // 6. set httpPost Entity
-        httpPost.setEntity(se);
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
 
-        // 7. Set some headers to inform server about the type of the content
-        httpPost.setHeader("Accept", "application/json");
-        httpPost.setHeader("Content-type", "application/json");
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
 
-        // 8. Execute POST request to the given URL
-        HttpResponse httpResponse = httpclient.execute(httpPost);
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
 
-        // 9. receive response as inputStream
-        inputStream = httpResponse.getEntity().getContent();
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
 
-        // 10. convert inputstream to string
-        if(inputStream != null)
-            result = convertInputStreamToString(inputStream);
-        else
-            result = "Did not work!";
+            // 10. convert inputstream to string
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
 
-    } catch (Exception e) {
-        Log.d("InputStream", e.getLocalizedMessage());
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
     }
 
-    // 11. return result
-    return result;
-    }
-    
     @Override
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
-        Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-        Log.d(TAG,result);
+        Toast.makeText(contextActivity, "Received!", Toast.LENGTH_LONG).show();
+        Log.d(TAG, result);
     }
     
     public static HttpClient getNewHttpClient() {
